7397ce47fb6451b4d25080f3105c036b1eedcee9
MaksGorobets <gorobets542@gmail.com>
2024-03-21 14:39:56 +0200

    Initial Commit

---
+++ DiceRoll/DiceRollApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollApp.swift
+//  DiceRoll
+//
+//  Created by Maks Winters on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
